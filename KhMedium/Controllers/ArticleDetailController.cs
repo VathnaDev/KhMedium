a8180ec598304fcf8632e2becb12264caf9dc35f
Pisith Prak <pisithcreate@gmail.com>
2019-02-17 22:19:35 +0700

    Create Article Detail View

---
+++ KhMedium/Controllers/ArticleDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhMedium.Controllers
+{
+    public class ArticleDetailController : Controller
+    {
+        // GET: ArticleDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
