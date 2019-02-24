d5697014586710ccb1df7079d9146056e66dec5e
VathnaDev <vathnadev@gmail.com>
2019-02-24 12:37:35 +0700

    Added Search

---
+++ KhMedium/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KhMedium.Models.ViewModel;
+
+namespace KhMedium.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index(String q)
+        {
+            var model = new SearchViewModel();
+            model.Query = q;
+            model.Type = "stories";
+            return View(model);
+        }
+
+        public ActionResult Users(String q)
+        {
+            var model = new SearchViewModel();
+            model.Query = q;
+            model.Type = "stories";
+            return View(model);
+        }
+
+        public ActionResult Publications(String q)
+        {
+            var model = new SearchViewModel();
+            model.Query = q;
+            model.Type = "publication";
+            return View(model);
+        }
+       
+    }
+}
---
+++ KhMedium/Models/ViewModel/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KhMedium.Entities;
+
+namespace KhMedium.Models.ViewModel
+{
+    public class SearchViewModel
+    {
+        public String Query { get; set; }
+        public List<Author> Users { get; set; }
+        public List<Publication> Publications { get; set; }
+        public List<Article> Articles { get; set; }
+        public String Type { get; set; }
+    }
+}
