7f5881b0c7d7c2034ea24451dbf3de6080f5f9fd
VathnaDev <vathnadev@gmail.com>
2019-02-26 22:06:09 +0700

    Before Solove Conflict

--- KhMedium/Areas/Admin/Controllers/HomeController.cs
+++ KhMedium/Areas/Admin/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace KhMedium.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         // GET: admin/Home
