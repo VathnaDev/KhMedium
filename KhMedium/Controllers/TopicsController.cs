9d6bee237a154c1565dfefe0f12bacb9697ef94f
Chanbo <chanbo97@gmail.com>
2019-02-26 22:23:25 +0700

    add topic article

--- KhMedium/Controllers/TopicsController.cs
+++ KhMedium/Controllers/TopicsController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KhMedium.Data;
+using KhMedium.Entities;
+using KhMedium.Models.ViewModel;
 
 namespace KhMedium.Controllers
 {
     public class TopicsController : Controller
     {
+        readonly UnitOfWork _context = new UnitOfWork(new KhMediumEntities());
+
         // GET: Topics
-        public ActionResult Index()
+        public ActionResult Index(string topicId)
         {
-            return View();
+            var viewModel = new TopicArticlesViewModel()
+            {
+                LatestArticles = _context.Articles.GetArticlesByTopic(topicId),
+                FeatureArticle = _context.Articles.GetArticlesByTopic(topicId).FirstOrDefault() ?? new Article(),
+                PopularArticles = _context.Articles.GetArticlesByTopic(topicId),
+                RelatedTopics = new List<string>(),
+                Topic = _context.Topics.Get(topicId)
+            };
+        
+
+            return View(viewModel);
         }
     }
 }
