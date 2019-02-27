f7892199573aaea8032ec42a50fb5b060b10c801
Chanbo <chanbo97@gmail.com>
2019-02-27 23:10:42 +0700

    bind data to profile

---
+++ KhMedium/Models/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KhMedium.Entities;
+
+namespace KhMedium.Models.ViewModel
+{
+    public class ProfileViewModel
+    {
+        public Author Author { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
