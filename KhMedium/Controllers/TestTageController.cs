e3cae529540b1e48bbca2d7b679e2b9cbd8f6916
Dalin <eandalined@example.com>
2019-02-28 18:41:42 +0700

    Added Tag Selector

---
+++ KhMedium/Controllers/TestTageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhMedium.Controllers
+{
+    public class TestTageController : Controller
+    {
+        // GET: TestTage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
